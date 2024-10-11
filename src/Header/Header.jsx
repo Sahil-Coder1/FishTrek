78446605559d373c6dbb5140dd1b8e796b0894a6
Sahil-Coder1 <26maysahil@gmail.com>
2024-10-11 18:35:26 +0530

    new push

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,11 +4,12 @@ const Header = () => {
         <div>
             <div className="bg-marian-blue">
                 <div className="left flex p-2 ">
-                    <img src="./src/navigation.png" alt="logo" className="p-2"/>
+                    <img src="../src/navigation.png" alt="logo" className="p-2"/>
                     <p className="min-h-full content-center px-2 text-alice-blue font-bold text-2xl">FishTrek</p>
                 </div>
-                <div className="flex right-0 absolute top-0 p-2 w-1/2">
-                    <ul type='none' className="flex p-2 w-full justify-evenly text-powder-blue font-bold">
+                <div className="flex right-0 absolute top-0 p-2 w-1/2 ">
+                <img src="../src/hamberger.png" alt="logo" className="absolute right-0 top-0 bg-powder-blue content-center"/>
+                    <ul type='none' className="lg:flex p-2 w-full justify-evenly text-powder-blue font-bold hidden ">
                         <li className='hoverEffect'>Fishes</li>
                         <li className='hoverEffect'>Locations</li>
                         <li className='hoverEffect'>About Us</li>
